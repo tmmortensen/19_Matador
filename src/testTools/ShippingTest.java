190023c78cdd59624312c1daac8fe37443eeb2f2
Chris Johansen <chrisdons@gmail.com>
2014-01-13 13:40:05 +0100

    Fix konflikt..

--- src/testTools/ShippingTest.java
+++ src/testTools/ShippingTest.java
@@ -20,17 +20,11 @@ public class ShippingTest {
 		this.player = new Player(5000, "Anders And", 1);
 		this.owner = new Player(1000, "Andersine", 2);
 
-<<<<<<< HEAD:src/testTools/ShippingTest.java
-		this.gameBoard.setField(new Shipping("Shipping1", 0, gameBoard), 6);
-		this.gameBoard.setField(new Shipping("Shipping2", 0, gameBoard), 16);
-		this.gameBoard.setField(new Shipping("Shipping3", 0, gameBoard), 20);
-		this.gameBoard.setField(new Shipping("Shipping4", 0, gameBoard), 21);
-=======
+
 		//this.gameBoard.setField(new Shipping("Fleet1", 0, gameBoard), 6);
 		//this.gameBoard.setField(new Shipping("Fleet2", 0, gameBoard), 16);
 		//this.gameBoard.setField(new Shipping("Fleet3", 0, gameBoard), 20);
 		//this.gameBoard.setField(new Shipping("Fleet4", 0, gameBoard), 21);
->>>>>>> 52d3e9d0f065c988a3dcc2fec68cda2c9e9fcb26:src/testTools/FleetTest.java
 
 		this.owner.setLocation(6);
 		//this.gameBoard.setOwner(owner);
@@ -42,15 +36,10 @@ public class ShippingTest {
 	public void tearDown() throws Exception {
 		this.player = new Player(5000, "Anders And", 1);
 		this.owner = new Player(1000, "Andersine", 2);
-<<<<<<< HEAD:src/testTools/ShippingTest.java
-		this.gameBoard.setField(new Shipping("Shipping2", 0, gameBoard), 19);
-		this.gameBoard.setField(new Shipping("Shipping3", 0, gameBoard), 20);
-		this.gameBoard.setField(new Shipping("Shipping4", 0, gameBoard), 21);
-=======
+
 		//this.gameBoard.setField(new Shipping("Fleet2", 0, gameBoard), 19);
 		//this.gameBoard.setField(new Shipping("Fleet3", 0, gameBoard), 20);
 		//this.gameBoard.setField(new Shipping("Fleet4", 0, gameBoard), 21);
->>>>>>> 52d3e9d0f065c988a3dcc2fec68cda2c9e9fcb26:src/testTools/FleetTest.java
 	}
 
 	@Test
